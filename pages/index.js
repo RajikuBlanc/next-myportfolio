742c51abc58a01b068e615afaca0024c517e4d91
本馬遼太郎 <rajiku.blanc@gmail.com>
2021-08-06 10:20:32 +0900

    commit link

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,24 @@
+import Link from 'next/link';
 export default function Home() {
+  const products = [{ name: 'bag', color: 'green' }, { name: 'shoes', color: 'red' }, { name: 'hat', color: 'blue' }];
   return (
     <div>
+      <ul>
+        <li>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </li>
+        {products.map(product => {
+          return (
+            <li key={product.name}>
+              <Link href={`/products/${product.name}/${product.color}`}>
+                <a>{product.name}</a>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
       <h1>Hello Next</h1>
     </div>
   );
